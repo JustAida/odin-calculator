2dcd8904d404f6d187dbb3b794efd23b6e74f647
JustAida <suradet40@gmail.com>
2022-02-14 06:02:27 +0700

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,18 @@
+// Fucntion to change the display number.
+const display = document.querySelector('.display-number');
+
+// Functions for basic math operators.
+
+// Function that takes two numbers and one operator then calls the above functions. 
+
+// Function to change the display when a number is clicked.
+
+// Store the display value in a variable.
+
+// Store the first number when the user presses an operator.
+
+// Save which operation has been chosen and the second number.
+
+// Call operate() on them after the user presses "=" key.
+
+// Update the display with the result of the operation.
